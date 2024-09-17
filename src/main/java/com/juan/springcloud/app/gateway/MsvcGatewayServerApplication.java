8605640d82563def3d7c0c31de5ab89b0e142815
jczorrilla <jczorrilla@tecnoquimicas.com>
2024-09-17 17:44:55 -0500

    feat: Inicializa el proyecto MicroserviceZero-Gateway
    - Agregadas dependencias de Reactive Gateway, DevTools y Eureka Discovery Client.
    - Configurado el puerto por defecto en 8090 para el gateway.
    - Añadida la URL del servicio Eureka en pplication.properties: eureka.client.serviceUrl.defaultZone=http://localhost:8761/eureka.

---
+++ src/main/java/com/juan/springcloud/app/gateway/MsvcGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.juan.springcloud.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcGatewayServerApplication.class, args);
+	}
+
+}
